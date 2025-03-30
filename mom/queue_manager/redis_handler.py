039cf2840b559c41ffa12fc2bb59191cb28e9bcd
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-29 21:26:24 -0500

    refactor: delete unuser methods

--- mom/queue_manager/redis_handler.py
+++ mom/queue_manager/redis_handler.py
@@ -10,40 +10,47 @@ class RedisHandler:
         self.client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
     
     def save_pending_request(self, service, client_id, task_id, payload, ttl=5000):
-        queue_key = f"queue:{service}"
-        data_key = f"pending:{service}:{client_id}:{task_id}"
-        value = json.dumps({
-            "task_id": task_id,
-            "client_id": client_id,
-            "service": service,
-            "payload": payload,
-            "status": "pending",
-            "created_at": datetime.utcnow().isoformat()
-        })
-        self.client.setex(data_key, ttl, value)
-        self.client.rpush(queue_key, data_key)
+        try:
+            queue_key= f"queue:{service}"
+            value = json.dumps({
+                "task_id": task_id,
+                "client_id": client_id,
+                "service": service,
+                "payload": payload,
+                "status": "pending",
+                "created_at": datetime.utcnow().isoformat()
+            })
+            self.client.rpush(queue_key, value)
+            return True,None
+        except Exception as e:
+            return False, str(e)
 
-    def pop_next_task_key(self, service):
-        queue_key = f"queue:{service}"
-        return self.client.lpop(queue_key)
-    
-    def get_task_data(self, task_key):
-        data = self.client.get(task_key)
-        return json.loads(data) if data else None
 
     def delete_task(self, task_key):
-        self.client.delete(task_key)
+        try:
+            self.client.delete(task_key)
+            return True, None
+        except Exception as e:
+            return False, str(e)
     
     def save_response(self, task_id, client_id, response, ttl=5000):
-        key = f"response:{client_id}:{task_id}"
-        value = json.dumps({
-            "status": "completed",
-            "response": response,
-            "timestamp": datetime.utcnow().isoformat()
-        })
-        self.client.setex(key, ttl, value)
+        try:
+            key = f"response:{client_id}:{task_id}"
+            value = json.dumps({
+                "status": "completed",
+                "response": response,
+                "timestamp": datetime.utcnow().isoformat()
+            })
+            self.client.setex(key, ttl, value)
+            return True,None
+        except Exception as e:
+            return False, str(e)
 
     def get_response(self, task_id, client_id):
-        key = f"response:{client_id}:{task_id}"
-        data = self.client.get(key)
-        return json.loads(data) if data else None
+        try:
+            key = f"response:{client_id}:{task_id}"
+            data = self.client.get(key)
+            response = json.loads(data) if data else None
+            return True, response, None
+        except Exception as e:
+            return False, None, str(e) 
