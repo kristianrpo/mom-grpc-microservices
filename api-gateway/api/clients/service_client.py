2321f9773a51453b8830fab15ae570e8dce4a79f
SaraCmque <saramanrique2016@gmail.com>
2025-04-05 12:45:16 -0500

    clients with grpc connection added

--- api-gateway/api/clients/service_client.py
+++ api-gateway/api/clients/service_client.py
@@ -11,7 +11,6 @@ class ServiceClient:
         
         self.channel = grpc.insecure_channel(f"{self.config['host']}:{self.config['port']}")
         
-        # Importar dinámicamente el stub (ej: calculator_pb2_grpc)
         grpc_module = __import__(
             f"generated.{service_name.lower()}_pb2_grpc",
             fromlist=[self.config["stub"]]
@@ -23,10 +22,8 @@ class ServiceClient:
         if method_name not in self.config["methods"]:
             raise ValueError(f"Method '{method_name}' not found in {self.service_name}")
         
-        # Convertir JSON a protobuf
         request_class = getattr(self.proto_module, self.config["methods"][method_name]["request"])
         request = ParseDict(payload, request_class())
         
-        # Llamar al método gRPC
         method = getattr(self.stub, method_name)
         return method(request)
